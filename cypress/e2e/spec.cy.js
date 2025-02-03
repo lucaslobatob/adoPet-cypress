a003be181a6051fde5f019e8e42baea7a91dd17b
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-03 08:17:34 -0300

    Fist test with cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+  })
+}) // Aqui se inicia o teste e o cypress irá visitar a página do site
+
