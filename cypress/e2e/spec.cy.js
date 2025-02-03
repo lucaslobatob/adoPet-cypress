db6974e8ff4d8366596a6b552a9ddb77a25b593a
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-03 14:53:47 -0300

    First cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+describe('Cadastrando um usuário na página adoPet', () => {
+  it('Preenchendo o formulário de cadastro', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/'); // Aqui se inicia o teste e o cypress irá visitar a página do site
+    cy.contains('a', 'Cadastrar').click(); // Aqui o cypress irá clicar no link de cadastro
+    cy.get('input[name="nome"]').type('Teste'); // Aqui o cypress irá preencher o campo "nome"
+    cy.get('input[name="email"]').type('teste@teste.com'); // Aqui se preenche o campo "email"
+    cy.get('input[name="password"]').type('Teste123'); // Aqui se preenche o campo "senha"
+    cy.get('input[name="confirm_password"]').type('Teste123'); // Aqui se preenche o campo "confirmar senha"
+    cy.contains('button', 'Cadastrar').click(); // Aqui o cypress irá clicar no botão de cadastro
   })
-}) // Aqui se inicia o teste e o cypress irá visitar a página do site
+})
 
