cb11ce8d204f14e65a31065fcc03658ed45ee36a
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-07 15:21:50 -0300

    create invalid login test

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,7 +1,16 @@
 describe('Página de login', () => {
     beforeEach(() => {
-        cy.visit('https://adopet-frontend-cypress.vercel.app/'); // Aqui se inicia o teste e o cypress irá visitar a página do site
+        cy.visit('https://adopet-frontend-cypress.vercel.app'); // Aqui se inicia o teste e o cypress irá visitar a página do site
         cy.get('[data-test="login-button"]').click(); // Aqui o cypress irá clicar no link de login
+        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+            statusCode: 400, // Aqui o cypress irá verificar se o status code é 400
+        }).as('stubPost');
+    })
+
+    it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+        cy.login('lucas@email.com', 'Senha123');
+        cy.wait('@stubPost');
+        cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible');
     })
 
     it('Verifica mensagem de falha ao fazer login', () => {
