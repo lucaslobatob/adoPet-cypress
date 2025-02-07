2b11779ec81be37a19fd3734a4ffc399d9424578
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-07 15:21:34 -0300

    create api message test

---
+++ cypress/e2e/api-mensagens.cy.js
@@ -0,0 +1,15 @@
+describe('Api Adopet', () => {
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTcwOTA0MTMwNSwiZXhwIjoxNzA5MzAwNTA1fQ.nhiaUyKdvN8RVxCkvne2gyI5n_pYnY_OMMdogMMqOlc`
+
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+            headers: { authorization }
+        })
+    }).then((res) => {
+        expect(res.status).to.be.equal(200)
+        expect(res.body).is.not.empty
+        expect(res.body).to.have.property('msg')
+    })
+})
