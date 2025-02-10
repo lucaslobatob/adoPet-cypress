a02bef8a8c087de1b1a45ab5f59b3eae53bd4f81
Lucas Lobato <lucasbiazotto2009@hotmail.com>
2025-02-10 08:06:20 -0300

    create cadastro em massa test

---
+++ cypress/e2e/cadastro-massa.cy.js
@@ -0,0 +1,17 @@
+import { users } from "../fixtures/users.json"
+
+describe('Página de cadastro', () => {
+    beforeEach(() => {
+        cy.visit('https://adopet-frontend-cypress.vercel.app');
+        cy.get('[data-test="register-button"]').click();
+    })
+    users.forEach(user => {
+        it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+            cy.get('[data-test="input-name"]').clear().type(user.name);
+            cy.get('[data-test="input-email"]').clear().type(user.email);
+            cy.get('[data-test="input-password"]').clear().type(user.password);
+            cy.get('[data-test="input-confirm-password"]').clear().type(user.password);
+            cy.get('[data-test="submit-button"]').click();
+        })
+    })
+})
